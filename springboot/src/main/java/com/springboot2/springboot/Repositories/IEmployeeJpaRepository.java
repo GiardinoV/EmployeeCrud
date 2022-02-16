b28aa693da88ae2a0e97287340248e088857b125
virginia <vgiardino@outlook.es>
2022-02-16 17:54:38 -0300

    Agrego clases test

--- springboot/src/main/java/com/springboot2/springboot/Repositories/IEmployeeJpaRepository.java
+++ springboot/src/main/java/com/springboot2/springboot/Repositories/IEmployeeJpaRepository.java
@@ -1,11 +1,13 @@
 package com.springboot2.springboot.Repositories;
 
+
 import com.springboot2.springboot.Models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IEmployeeJpaRepository extends JpaRepository<Employee, Long> {
-    Employee findByEmployeeid(String employeeId);
+    Employee findByEmployeeId(String employeeId);
+
 
 }
--- springboot/src/test/java/com/springboot2/springboot/EmployeeJpaRepositoryTest.java
+++ springboot/src/test/java/com/springboot2/springboot/EmployeeJpaRepositoryTest.java
@@ -4,17 +4,20 @@ import com.springboot2.springboot.Models.Employee;
 import com.springboot2.springboot.Repositories.IEmployeeJpaRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+//@AutoConfigureTestDatabase (connection = EmbeddedDatabaseConnection.H2) M
 public class EmployeeJpaRepositoryTest {
     @Autowired
     private IEmployeeJpaRepository repo;
-
+    
     @Test
     public void saveEmployee(){
         Employee vir = new Employee("Vir", "Giardino", "v123");
